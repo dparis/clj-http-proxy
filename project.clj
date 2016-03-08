f55ab27afa7e746ada3d3536ba5619f663a00313
Dylan Paris <dylan.paris+github@gmail.com>
2016-03-08 12:20:27 -0800

    Update LICENCE and project.clj description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-http-proxy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "An HTTP proxy server based on Aleph"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT Licence"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
