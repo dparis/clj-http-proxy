fa66c5ef112e6aa01010e64945c8079610459dcf
Dylan Paris <dylan.paris+github@gmail.com>
2016-03-08 12:14:21 -0800

    Initial commit

---
+++ test/clj_http_proxy/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-http-proxy.core-test
+  (:require [clojure.test :refer :all]
+            [clj-http-proxy.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
